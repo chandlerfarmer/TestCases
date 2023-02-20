1e96280d87713e9a790b5532f316705f109baf40
chandlerfarmer <chandlerfarmer57@gmail.com>
2023-02-20 13:11:07 -0500

    test

--- juiceshop_test.py
+++ juiceshop_test.py
@@ -6,7 +6,11 @@ from scapy.all import *
 def packet_handler(packet):
     print(packet.show())
 
-sniff(iface="docker0", filter="tcp port 80", prn=packet_handler, count=10)
+print(show_interfaces())
+
+#sniff(iface="docker0", filter="tcp port 80", prn=packet_handler, count=10)
+
+
 class TestOWASPJuiceShop(unittest.TestCase):
 
     def test_sql_injection(self):
