b55f15963ba9a64f824e14310790022e42e39fbf
chandlerfarmer <chandlerfarmer57@gmail.com>
2023-02-20 13:41:41 -0500

    test

--- juiceshop_test.py
+++ juiceshop_test.py
@@ -1,23 +1,35 @@
 import unittest # Used to execute the unit tests
 import requests # Used for HTTP & API Calls
-from scapy.all import *
+import pyshark
 import json
 
-def packet_handler(packet):
-    if packet.haslayer(Raw):
+# Set the source and destination addresses to filter on
+src_addr = "172.17.0.1"
+dst_addr = "172.17.0.2"
+interface = 'docker0'
+
+# Create a Tshark capture object with the filter expression
+capture = pyshark.LiveCapture(interface=interface, display_filter=f"ip.src == {src_addr} and ip.dst == {dst_addr}")
+
+# Start the capture
+capture.sniff()
+
+# Loop through the captured packets
+for packet in capture:
+
+    # Extract the payload of the packet
+    payload = packet.payload
+
+    # Check if the payload contains the specific JSON payload you're looking for
+    if "specific json payload" in payload:
         
-        # Try to parse the payload as JSON
-        try:
-            json_data = json.loads(packet[Raw].load.decode())
-            # Print the JSON data
-            print(json.dumps(json_data, indent=4))
-        except json.JSONDecodeError:
-            # Payload is not JSON
-            pass
+        # Parse the JSON data from the payload
+        json_data = json.loads(payload)
         
+        # Analyze the JSON data as needed
+        # ...
 
 
-sniff(iface="docker0", filter="src 172.17.0.1 and dst 172.17.0.2", prn=packet_handler)
 
 
 class TestOWASPJuiceShop(unittest.TestCase):
