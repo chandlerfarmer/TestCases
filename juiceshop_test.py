01ad6590f26a66f93d4ef9f4af311bc370a14935
chandlerfarmer <chandlerfarmer57@gmail.com>
2023-02-22 20:05:50 -0500

    y

--- juiceshop_test.py
+++ juiceshop_test.py
@@ -95,7 +95,7 @@ class TestOWASPJuiceShop(unittest.TestCase):
     def test_weak_password_requirements(self):
         url = "http://localhost:3000/api/Users/"
         payload = { # Payload for a new unique user (must change each run)
-            "email": "test20222222222222222222222222222022222@test.com",
+            "email": "test20222222222222222222222222222222022222@test.com",
             "password": "12345",
             "passwordRepeat": "12345",
             "securityAnswer": "mom",
@@ -141,12 +141,12 @@ class TestOWASPJuiceShop(unittest.TestCase):
              "Authorization": "Bearer " + adminToken,
         }
         response = requests.post(url+f"/api/BasketItems/", data={"BasketId": "1", "ProductId": 1, "quantity": 1})
-        #json_data = response.json()
-        #id_value = json_data['status']
-        #print(json_data)
+        json_data = response.json()
+        id_value = json_data['data']['id']
+ 
 
-       # response = requests.put(url+f"/api/BasketItems/58", headers=adminHeaders, json={"quantity": -10})
-       # self.assertNotEqual(response.status_code, 200)
+        response = requests.put(url+f"/api/BasketItems/{id_value}", headers=adminHeaders, json={"quantity": -10})
+        self.assertNotEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
