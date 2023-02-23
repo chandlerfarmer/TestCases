16ae93df131910b9fb15fae691cacbac54cab38f
chandlerfarmer <chandlerfarmer57@gmail.com>
2023-02-22 20:30:44 -0500

    y

--- juiceshop_test.py
+++ juiceshop_test.py
@@ -95,7 +95,7 @@ class TestOWASPJuiceShop(unittest.TestCase):
     def test_weak_password_requirements(self):
         url = "http://localhost:3000/api/Users/"
         payload = { # Payload for a new unique user (must change each run)
-            "email": "test2022222222222222222222222222222222222222222022222@test.com",
+            "email": "test202222222222222222222222222222222222222222222022222@test.com",
             "password": "12345",
             "passwordRepeat": "12345",
             "securityAnswer": "mom",
@@ -146,14 +146,13 @@ class TestOWASPJuiceShop(unittest.TestCase):
         }
 
         response = requests.post(url+f"/api/BasketItems/", headers=adminHeaders, data={"BasketId": "1", "ProductId": 1, "quantity": 1})
-
-        json_content = response.json()
+        real_response = requests.get(url+f"/rest/basket/1", headers=adminHeaders)
+        json_content = real_response.json()['data']['Products']['0']
         print(json_content)
 
         # Access the 'id' field of the 'data' object
-        id_value = json_content['data']['id']
 
-        response = requests.put(url+f"/api/BasketItems/{id_value}", headers=adminHeaders, json={"quantity": -10})
+        response = requests.put(url+f"/api/BasketItems/{json_content}", headers=adminHeaders, json={"quantity": -10})
         self.assertNotEqual(response.status_code, 200)
 
 
