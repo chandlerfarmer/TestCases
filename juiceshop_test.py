da1cf3221befbe77178f85f099f46f3a2b30352d
chandlerfarmer <chandlerfarmer57@gmail.com>
2023-02-22 20:49:55 -0500

    test

--- juiceshop_test.py
+++ juiceshop_test.py
@@ -95,7 +95,7 @@ class TestOWASPJuiceShop(unittest.TestCase):
     def test_weak_password_requirements(self):
         url = "http://localhost:3000/api/Users/"
         payload = { # Payload for a new unique user (must change each run)
-            "email": "tester1022@test.com",
+            "email": "tester10222@test.com",
             "password": "12345",
             "passwordRepeat": "12345",
             "securityAnswer": "mom",
@@ -148,7 +148,7 @@ class TestOWASPJuiceShop(unittest.TestCase):
         response = requests.post(url+f"/api/BasketItems/", headers=adminHeaders, data={"BasketId": "1", "ProductId": 1, "quantity": 1})
         real_response = requests.get(url+f"/rest/basket/1", headers=adminHeaders)
         json_content = real_response.json()
-        val = json_content['data']['BasketItem'][9]['id']['ProductId']
+        val = json_content['data']['Products'][0]['BasketItem']['ProductId']
         print('HERE IS VALUE:', val)
 
         # Access the 'id' field of the 'data' object
